b5547f68af2679604003b7d1ed21b4b5883c3a9b
ronen.goren <ronen.goren@gmail.com>
2018-06-10 18:14:04 -0400

    deploying test

--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,13 @@ ActiveRecord::Schema.define(version: 2018_06_10_171431) do
     t.datetime "updated_at"
   end
 
+  create_table "tests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "paired_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
