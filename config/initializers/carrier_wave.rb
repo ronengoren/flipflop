b9337f14c941ca55ea15382b8aa34050574d13a7
ronen.goren <ronen.goren@gmail.com>
2018-06-09 21:24:55 -0400

    make it better

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,14 +3,15 @@ require 'carrierwave/orm/activerecord'
 
 
 if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.fog_credentials = {
-        # Configuration for Amazon S3
-        :provider              => 'AWS',
-        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-        :region                => ENV['S3_REGION']
-      }
-      config.fog_directory     =  ENV['S3_BUCKET']
-    end
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+        provider:              'AWS',
+        aws_access_key_id:     ENV["aws_access_id"],
+        aws_secret_access_key: ENV["aws_access_secret_key"],
+        region: 'us-east-2'
+    }
+    config.storage = :fog
+    config.fog_directory  = "flipflop.com"
+    config.fog_public     = false
+  end
   end
