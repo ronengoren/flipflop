fb400a268af8f5e22ccc87056cdbbfcc5845988d
ronen.goren <ronen.goren@gmail.com>
2018-06-13 23:14:04 -0400

    changes js to card display only when fully load

--- app/assets/javascripts/flipflop.js
+++ app/assets/javascripts/flipflop.js
@@ -104,9 +104,7 @@ $('document').ready(function() {
             trigger: "manual", // click, hover or manual
             speed: 500
         });
-        setTimeout(function() {
-            $(".flip").flip(true);
-        }, 15000);
+
 
     });
 
